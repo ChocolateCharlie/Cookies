97a7567e4ef1ce3ee15f28f573b73735284682fd
ChocolateCharlie <aurore.amrit@gmail.com>
2018-01-06 15:39:16 +0100

    Set Data on Views (step 22) : Hook up the button (step 3)

--- app/src/main/java/com/example/cookies/MainActivity.java
+++ app/src/main/java/com/example/cookies/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cookies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.cookies.R;
 
@@ -20,8 +22,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public void eatCookie(View view) {
         // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
+        imageView.setImageResource(R.drawable.after_cookie);
 
         // TODO: Find a reference to the TextView in the layout. Change the text.
+        TextView textView = (TextView) findViewById(R.id.status_text_view);
+        textView.setText("I'm so full");
 
     }
 }
